b2e0b4a7e05559760869a838458b377ddae5b53d
Jason Daniel Augustine Gilliland <jdagilliland@gmail.com>
2014-11-05 20:54:45 -0500

    Make minor revisions to skeleton files
    Also add in a sample FASTA file to use for BLAST searches.

--- hairpin.m
+++
@@ -1,5 +0,0 @@
-function score = hairpin(sequence, salt_content)
-% Given a sequence for which we want to determine the tendency to form
-% hairpins, as well as a salt content, we generate a score that characterizes
-% the tendency for hairpinning.
-end
