28f8d032e2ee6add7938193df3ad19da635c9d9d
ssivilich <ssivilich@gmail.com>
2014-11-17 18:59:37 -0500

    Update gc_count.m
    added /numel(sequence)
    
    Does this complete this function?

--- gc_count.m
+++ gc_count.m
@@ -1,5 +1,7 @@
 function content = gc_count(sequence)
 %% This function takes a sequence and returns the gc content as a fraction
+
+%what does the lower(sequence) mean?  Is that an index of sequence?
   sequence = lower(sequence);
-  content = sum(sequence == 'g' | sequence == 'c');
+  content = sum(sequence == 'g' | sequence == 'c')/numel(sequence);
 end
