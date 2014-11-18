e9df92d9605380cc0cb576b65ba769ae8c4a11bd
Jason Daniel Augustine Gilliland <jdagilliland@gmail.com>
2014-11-18 12:47:27 -0500

    Fix comment in gc_count
    This resolves recent pull request by Sarah.

--- gc_count.m
+++ gc_count.m
@@ -1,5 +1,5 @@
 function content = gc_count(sequence)
-%% This function takes a sequence and returns the gc content as a fraction
+%% This function takes a sequence and returns the gc content as an integer
   sequence = lower(sequence);
   content = sum(sequence == 'g' | sequence == 'c');
 end
