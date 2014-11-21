5949f03c22b7ed6810e89090d61ee0b3279779ac
Jason Daniel Augustine Gilliland <jdagilliland@gmail.com>
2014-11-21 18:44:56 -0500

    Clean up obsolete functions
    These are no longer necessary, or the functionality if covered by other
    functions.

--- melting_point.m
+++
@@ -1,7 +0,0 @@
-function Tm = melting_point(sequence, salt_content)
-%% This function computes the melting point of a sequence given the salt
-%content.
-
-
-%%% blah blah code that computed melting point
-end
